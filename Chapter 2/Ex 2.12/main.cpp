0dcd611b4e030ca84d1a0c55c683ee6cad3dbed8
unknown <italo.lujanpedreschi@gmail.com>
2023-08-01 23:50:24 -0700

    Add existing exercise files

---
+++ Chapter 2/Ex 2.12/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+  // int double = 3.14;
+  int _; // should not begin with underscore if outside of a function
+  // int catch-22; // no hyphens
+  // int 1_or_2 = 1; // must begin with a letter or underscore
+  double Double = 3.14; // should be lowercase but is not invalid
+  
+  return 0;
+}
---
+++ Chapter 2/Ex 2.9/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+  // std::cin >> int input_value;
+  int input_value;
+  std::cin >> input_value;
+
+  // double salary = wage = 9999.99;
+  double salary = 9999.99, wage = salary;
+  std::cout << salary << std::endl;
+  std::cout << wage << std::endl;
+  return 0;
+}
